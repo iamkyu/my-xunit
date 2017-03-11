dc9842fcb08eff560213ab988ed9471d55032421
kjnam <kyujnam@gmail.com>
2017-03-11 14:04:49 +0900

    Finished chapter 13

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -34,7 +34,40 @@ public class DollarTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    static class Money {
+    @Test
+    public void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
+
+    @Test
+    public void testPlusReturnSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    static class Money implements Expression {
         protected int amount;
         protected String currency;
 
@@ -64,6 +97,40 @@ public class DollarTest {
 
         public String currency() {
             return currency;
+        }
+
+        public Expression plus(Money addend) {
+            return new Sum(this, addend);
+        }
+
+        @Override
+        public Money reduce(String to) {
+            return this;
+        }
+    }
+
+    private class Bank {
+        public Money reduce(Expression source, String to) {
+            return source.reduce(to);
+        }
+    }
+
+    interface Expression {
+        Money reduce(String to);
+    }
+
+    private static class Sum implements Expression {
+        public Money augend;
+        public Money addend;
+
+        public Sum(Money augend, Money addend) {
+            this.augend = augend;
+            this.addend = addend;
+        }
+
+        public Money reduce(String to) {
+            int amount = augend.amount + addend.amount;
+            return new Money(amount, to);
         }
     }
 }
