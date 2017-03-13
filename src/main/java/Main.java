91acbc7466bbe66effa0c614114585920eb3b36d
kjnam <kyujnam@gmail.com>
2017-03-13 19:47:00 +0900

    Reset All

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,5 @@
  */
 public class Main {
     public static void main(String[] args) {
-        TestSuite suite = new TestSuite();
-        suite.add(new TestCaseTest("testResult"));
-        suite.add(new TestCaseTest("testFailedResult"));
-        suite.add(new TestCaseTest("testFailedResultFormatting"));
-
-        TestResult result = new TestResult();
-        suite.run(result);
-        result.summary();
     }
 }
