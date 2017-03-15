f99ded6ab5aad90d3665dc4c560db682f7c4fba3
iamkyu <kyujnam@gmail.com>
2017-03-15 09:01:30 +0900

    Finished chapter 2~9

--- readme.md
+++ readme.md
@@ -9,5 +9,5 @@
 - [테스트 주도 개발|저자 켄트 벡|역자 김창준, 강규영|인사이트 |2014.02.15](http://book.naver.com/bookdb/book_detail.nhn?bid=7443642) 18장 ~ 24장
 - [토비님 블로그: 테스팅 프레임워크는 직접 만들어 써보자](http://toby.epril.com/?p=424)
 - [백기선님 블로그:  TDDBE - xUnit](http://whiteship.tistory.com/1866)
-- [박소윤님 Github - xUnit](https://github.com/skyhills13/xUnit/)
+- [박소은님 Github - xUnit](https://github.com/skyhills13/xUnit/)
 - [김용훈님 블로그: [번역]JUnit A Cook’s Tour](https://bluepoet.me/2016/12/03/번역junit-a-cooks-tour/)
--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -1,7 +1,94 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * @author Kj Nam
  * @since 2017-03-15
  */
 public class DollarTest {
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+
+        assertTrue (Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    private static abstract class Money {
+        protected int amount;
+        protected String currency;
+
+        Money(int amount, String currency) {
+            this.amount = amount;
+            this.currency = currency;
+        }
+
+        public abstract Money times(int multiplier);
+
+        public String currency() {
+            return currency;
+        }
+
+        public static Money dollar(int amount) {
+            return new Dollar(amount, "USD");
+        }
+
+        public static Money franc(int amount) {
+            return new Franc(amount, "CHF");
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Money money = (Money) obj;
+            return amount == money.amount
+                    && getClass().equals(money.getClass());
+        }
+
+    }
+
+    private static class Dollar extends Money {
+        public Dollar(int amount, String currency) {
+            super(amount, currency);
+        }
+
+        public Money times(int multiplier) {
+            return Money.dollar(amount * multiplier);
+        }
+
+    }
+
+    private static class Franc extends Money {
+        public Franc(int amount, String currency) {
+            super(amount, currency);
+        }
+
+        public Money times(int multiplier) {
+            return Money.franc(amount * multiplier);
+        }
+    }
 
 }
